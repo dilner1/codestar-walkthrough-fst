bd10fdc3f00717aa2c5712f5208587664f461200
dilner1 <dan.milner871@gmail.com>
2022-02-08 17:51:28 +0000

    Heroku working :)

--- codestar/settings.py
+++ codestar/settings.py
@@ -130,6 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
 # added after 
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
