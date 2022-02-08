1a57250c1de561403212aa86d90a9cd5206b56c1
dilner1 <dan.milner871@gmail.com>
2022-02-08 17:11:18 +0000

    removing . in STATIC_ROOT

--- codestar/settings.py
+++ codestar/settings.py
@@ -133,7 +133,7 @@ STATIC_URL = '/static/'
 # added after
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join.(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # also added
 MEDIA_URL = '/media/'
