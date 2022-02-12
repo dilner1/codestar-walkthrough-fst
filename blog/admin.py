d20b8d5cdd592d1d425e2c27a38675ee3a7ada9f
dilner1 <dan.milner871@gmail.com>
2022-02-12 19:00:17 +0000

    updating comment in admin

--- blog/admin.py
+++ blog/admin.py
@@ -11,7 +11,12 @@ class PostAdmin(SummernoteModelAdmin):
     list_filter = ('status', 'created_on')
     summernote_fields = ('content')
     
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
     
+    list_display = ['name', 'body', 'post', 'created_on', 'approved']
+    list_filter = ['approved', 'created_on']
+    search_fields = ['name', 'email', 'body']
 
 # Register your models here.
 # admin.site.register(Post)
