f13dd97d95963cc7e8250c01ddc3c6c943c8a88a
dilner1 <dan.milner871@gmail.com>
2022-02-12 18:13:18 +0000

    adding to admine

--- blog/admin.py
+++ blog/admin.py
@@ -5,7 +5,13 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
 
+    search_fields = ['title', 'content']
+    list_display = ['title', 'slug', 'status', 'created_on' ]
+    prepopulated_fields = {'slug': ('title', )}
+    list_filter = ('status', 'created_on')
     summernote_fields = ('content')
+    
+    
 
 # Register your models here.
 # admin.site.register(Post)
