c23936bfc77e5d4688241010f56ceff7dce9cd79
dilner1 <dan.milner871@gmail.com>
2022-02-13 23:01:03 +0000

    not quite working ay

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views import generic
-from .models import post
+from .models import Post
 
 # Create your views here.
 class PostList(generic.ListView):
