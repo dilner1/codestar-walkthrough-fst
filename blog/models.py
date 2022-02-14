7b48f245575e501101141e65ce4dac4b6ddb590a
dilner1 <dan.milner871@gmail.com>
2022-02-14 14:05:34 +0000

    post link not working

--- blog/models.py
+++ blog/models.py
@@ -10,23 +10,23 @@ class Post(models.Model):
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='blog_posts')
-    updated_on = models.DateTimeField(auto_now=True)
-    content = models.TextField()
     featured_image = CloudinaryField('image', default='placeholder')
     excerpt = models.TextField(blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
     likes = models.ManyToManyField(User, related_name='blogpost_like', blank=True)
 
-class Meta:
-    ordering = ['-created_on']
+    class Meta:
+        ordering = ['-created_on']
     # with minus shows items in descending order
 
-def __str__(self):
-    return self.title
+    def __str__(self):
+        return self.title
 
-def number_of_likes(self):
-    return self.likes.count()
+    def number_of_likes(self):
+        return self.likes.count()
 
 class Comment(models.Model):
     post = models.ForeignKey(
@@ -37,8 +37,8 @@ class Comment(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     approved = models.BooleanField(default=False)
 
-class Meta:
-    ordering = ['created_on']
+    class Meta:
+        ordering = ['created_on']
 
-def __str__(self):
-    return f'Comment {self.body} by {self.name}'
+    def __str__(self):
+        return f'Comment {self.body} by {self.name}'
