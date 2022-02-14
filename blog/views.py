bd55ca79052e9c9628527f837acdc60d82026338
dilner1 <dan.milner871@gmail.com>
2022-02-14 16:14:39 +0000

    latest commit workjing again

--- blog/views.py
+++ blog/views.py
@@ -2,13 +2,12 @@ from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 from .models import Post
 
-
+# Create your views here.
 class PostList(generic.ListView):
     model = Post
-    queryset = Post.objects.filter(status=1).order_by("-created_on")
-    template_name = "index.html"
+    query_set = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
     paginate_by = 6
-
 
 class PostDetail(View):
 
